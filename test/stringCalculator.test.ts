f6a35a2cb38fe1295f5377561e49c7ff0c1e78a7
Sourabh Shegane <sourabhshegane@Sourabhs-MacBook-Air.local>
2025-02-22 22:05:09 +0530

    test(stringCalculator): Add case to check if an error is thrown if number is not passed as input

--- test/stringCalculator.test.ts
+++ test/stringCalculator.test.ts
@@ -1,5 +1,9 @@
 import { add } from "../src/stringCalculator";
 
-test("returns 0 for an empty string", () => {
+test("Returns 0 for an empty string", () => {
     expect(add("")).toBe(0);
+});
+
+test("throws an error if the input is not a number", () => {
+    expect(() => add("sourabh")).toThrow("Invalid input: Not a number");
 });
