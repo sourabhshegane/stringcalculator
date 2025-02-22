aaf5135983cdaaf1beaac3298d574e322bc91570
Sourabh Shegane <sourabhshegane@Sourabhs-MacBook-Air.local>
2025-02-22 22:06:35 +0530

    refactor(stringCalculator): Throw error for non numerical input

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,4 +2,10 @@ export function add(input: string): number {
     if (input === '') {
         return 0;
     }
+
+    input.split(',').map(str => {
+        if (isNaN(Number(str))) {
+            throw new Error(`Invalid input: Not a number`);
+        }
+    });
 }
